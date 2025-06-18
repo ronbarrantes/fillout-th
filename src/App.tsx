834b3365c6ef2fa409d1283847aa47658799a392
Ron Barrantes <ronbarrantes@gmail.com>
2025-06-18 18:59:12 -0400

    Udpate the app and change header

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,20 @@
-import { useState } from "react";
+import { Button } from "@/components/ui/button";
 
-import reactLogo from "./assets/react.svg";
-import { Button } from "./components/ui/button";
-
-import viteLogo from "/vite.svg";
+const Nav = () => {
+  return (
+    <ul className="border border-green-500">
+      <li>
+        <Button>My Button</Button>
+      </li>
+    </ul>
+  );
+};
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <Button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </Button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="flex h-screen w-screen items-center justify-center overflow-hidden border border-red-500">
+      <Nav />
+    </div>
   );
 }
 
