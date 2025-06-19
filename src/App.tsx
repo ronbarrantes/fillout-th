8fa2b9a39df4e07efc86afdf9c0d2193052e1a52
Ron Barrantes <ronbarrantes@gmail.com>
2025-06-19 15:36:09 -0400

    Update icon styles

--- src/App.tsx
+++ src/App.tsx
@@ -6,30 +6,43 @@ import { Button as ButtonPrimitive } from "@/components/ui/button";
 type CustomButton = {
   children: React.ReactNode;
   className?: string;
+  iconClassName?: string;
 };
 
 const AddPageButton = () => {
   return (
-    <ButtonPrimitive className="!m-0 h-fit w-fit !rounded-full border !border-slate-300/60 !bg-white !p-0 text-sm !text-black">
+    <ButtonPrimitive
+      className={classNames(
+        "!m-0 h-fit w-fit !rounded-full border !border-slate-300/60 !bg-white !p-0 text-sm !text-black",
+        "hover:!border-slate-400 hover:!outline-none focus:!outline-none"
+      )}
+    >
       <Icon name="add" className="m-0 size-4 p-0.5" />
       <span className="sr-only">Add a page</span>
     </ButtonPrimitive>
   );
 };
 
-const Button = ({ children }: CustomButton) => {
+const Button = ({ children, iconClassName }: CustomButton) => {
   return (
     <ButtonPrimitive
       className={classNames(
-        "group flex gap-2 !rounded-sm border !bg-gray-100 !p-4 !pl-3 !text-sm !text-black !shadow-none",
-        "hover:!border-transparent hover:!bg-gray-200",
-        "focus:!border-blue-400 focus:!bg-white focus:!ring-2 focus:!ring-gray-300/60 focus:!outline-none",
+        "group flex gap-2 !rounded-sm border !bg-gray-100 !p-4 !pl-3 !text-sm !text-slate-500 !shadow-none",
+        "hover:!border-transparent hover:!bg-gray-200 hover:!text-slate-600",
+        "focus:!border-blue-400 focus:!bg-white focus:!text-black focus:!ring-2 focus:!ring-gray-300/60 focus:!outline-none",
         "active:!ring-none focus:!shadow-sm active:!border-gray-300/60 active:!bg-white active:!ring-0",
         classNames
       )}
     >
       {/* <Icon className="m-0 size-5 p-0 group-hover:text-amber-500" name="info" /> */}
-      <Icon className="m-0 size-5 p-0" name="info" />
+      <Icon
+        className={classNames(
+          "m-0 size-5 p-0",
+          "group-focus:text-amber-500",
+          iconClassName
+        )}
+        name="info"
+      />
       {children}
     </ButtonPrimitive>
   );
