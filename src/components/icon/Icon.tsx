3aef5a1c132d1c168585f3115f689fb790d7bc26
Ron Barrantes <ronbarrantes@gmail.com>
2025-06-20 12:10:09 -0400

    Remove unused component

--- src/components/icon/Icon.tsx
+++ src/components/icon/Icon.tsx
@@ -39,24 +39,3 @@ export const Icon = ({
 
   return comp;
 };
-
-export const UpDownArrowIcon = ({
-  direction,
-  className,
-  iconName = "arrow",
-}: {
-  direction?: "up" | "down";
-  className?: string;
-  iconName?: IconsLisType;
-}) => {
-  return (
-    <Icon
-      name={iconName}
-      className={classNames(
-        "transition-transform hover:!text-inherit",
-        direction === "down" ? "rotate-90" : "-rotate-90",
-        className
-      )}
-    />
-  );
-};
