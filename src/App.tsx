81dad60e159cb0f14f821fc4f21f2ae60fff17bb
Ron Barrantes <ronbarrantes@gmail.com>
2025-06-19 23:06:25 -0400

    Update the add page button

--- src/App.tsx
+++ src/App.tsx
@@ -28,49 +28,66 @@ type CustomButton = {
   iconClassName?: string;
 };
 
-const AddPageButton = ({ onClick }: { onClick: () => void }) => {
+const AddPageButton = ({
+  mode = "icon",
+  onClick,
+}: {
+  onClick: () => void;
+  mode?: "icon" | "label";
+}) => {
   return (
     <ButtonPrimitive
       onClick={onClick}
       className={classNames(
-        "!m-0 h-fit w-fit !rounded-full border !border-slate-300/60 !bg-white !p-0 text-sm !text-black",
-        "hover:!border-slate-400 hover:!outline-none focus:!outline-none"
+        "border !border-slate-300/60 !bg-white text-sm !text-black hover:!border-slate-400 hover:shadow-sm hover:!outline-none focus:!outline-none",
+        mode === "icon" && "!m-0 h-fit w-fit !rounded-full !p-0",
+        mode === "label" && "group flex gap-2 !rounded-md !p-4 !pl-3"
       )}
     >
-      <Icon name="add" className="m-0 size-4 p-0.5" />
-      <span className="sr-only">Add a page</span>
+      <Icon
+        name="add"
+        className={classNames(
+          "m-0",
+          mode === "icon" && "size-4 p-0.5",
+          mode === "label" && "size-5 p-0 group-hover:text-amber-500"
+        )}
+      />
+      <span className={classNames(mode === "icon" && "sr-only")}>Add page</span>
     </ButtonPrimitive>
   );
 };
 
-const Button = React.forwardRef<HTMLButtonElement, CustomButton>(
-  ({ style, children, iconClassName, className, onClick }, ref) => {
-    return (
-      <ButtonPrimitive
-        ref={ref}
-        style={style}
-        onClick={onClick}
+const Button = ({
+  style,
+  children,
+  iconClassName,
+  className,
+  onClick,
+}: CustomButton) => {
+  return (
+    <ButtonPrimitive
+      style={style}
+      onClick={onClick}
+      className={classNames(
+        "group flex gap-2 !rounded-md border !bg-gray-100 !p-4 !pl-3 !text-sm !text-slate-500 !shadow-none",
+        "hover:!border-transparent hover:!bg-gray-200 hover:!text-slate-600",
+        "focus:!border-blue-400 focus:!bg-white focus:!text-black focus:!ring-2 focus:!ring-gray-300/60 focus:!outline-none",
+        "active:!ring-none focus:!shadow-sm active:!border-gray-300/60 active:!bg-white active:!ring-0",
+        className
+      )}
+    >
+      <Icon
         className={classNames(
-          "group flex gap-2 !rounded-sm border !bg-gray-100 !p-4 !pl-3 !text-sm !text-slate-500 !shadow-none",
-          "hover:!border-transparent hover:!bg-gray-200 hover:!text-slate-600",
-          "focus:!border-blue-400 focus:!bg-white focus:!text-black focus:!ring-2 focus:!ring-gray-300/60 focus:!outline-none",
-          "active:!ring-none focus:!shadow-sm active:!border-gray-300/60 active:!bg-white active:!ring-0",
-          className
+          "m-0 size-5 p-0",
+          "group-focus:text-amber-500",
+          iconClassName
         )}
-      >
-        <Icon
-          className={classNames(
-            "m-0 size-5 p-0",
-            "group-focus:text-amber-500",
-            iconClassName
-          )}
-          name="info"
-        />
-        {children}
-      </ButtonPrimitive>
-    );
-  }
-);
+        name="info"
+      />
+      {children}
+    </ButtonPrimitive>
+  );
+};
 
 Button.displayName = "Button";
 
@@ -200,7 +217,7 @@ const Nav = () => {
             </React.Fragment>
           ))}
           <li>
-            <Button onClick={() => handleAddPage()}>Add Page</Button>
+            <AddPageButton mode="label" onClick={() => handleAddPage()} />
           </li>
         </ul>
       </SortableContext>
